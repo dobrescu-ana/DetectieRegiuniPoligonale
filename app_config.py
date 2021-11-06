3b00ca29549b21bcd2dbcf1594e9d444e7f6c43d
dobrescu-ana <46503870+dobrescu-ana@users.noreply.github.com>
2021-11-06 16:43:07 +0200

    Implemented lists to read input files, imported specific libraries.

---
+++ app_config.py
@@ -0,0 +1,43 @@
+lowLimit=0.7
+
+# Defined color legend for classes
+illustrationColor="blue"
+headlineColor="red"
+titleColor="yellow"
+textColor="green"
+
+# Defined paths for input/output data
+imagePath="images\\"
+resizedPath="images\\resized\\"
+csvPath="csvs\\"
+outputPath="output\\"
+
+originalImage=["242_0_2638730_0_5D1327B5.tif",
+                "242_10_2638730_19_5D132A6D.tif",
+                "242_12_2638730_20_5D132A8C.tif",
+                "242_14_2638730_22_5D132AD1.tif",
+                "242_16_2638730_24_5D132B0E.tif"]
+
+headlineCSV=["242_0_2638730_0_5D1327B5_gradient_headline.csv",
+                "242_10_2638730_19_5D132A6D_gradient_headline.csv",
+                "242_12_2638730_20_5D132A8C_gradient_headline.csv",
+                "242_14_2638730_22_5D132AD1_gradient_headline.csv",
+                "242_16_2638730_24_5D132B0E_gradient_headline.csv"]
+
+illustrationCSV=["242_0_2638730_0_5D1327B5_gradient_illustration.csv",
+                    "242_10_2638730_19_5D132A6D_gradient_illustration.csv",
+                    "242_12_2638730_20_5D132A8C_gradient_illustration.csv",
+                    "242_14_2638730_22_5D132AD1_gradient_illustration.csv",
+                    "242_16_2638730_24_5D132B0E_gradient_illustration.csv"]
+
+titleCSV=["242_0_2638730_0_5D1327B5_gradient_runningtitle.csv",
+            "242_10_2638730_19_5D132A6D_gradient_runningtitle.csv",
+            "242_12_2638730_20_5D132A8C_gradient_runningtitle.csv",
+            "242_14_2638730_22_5D132AD1_gradient_runningtitle.csv",
+            "242_16_2638730_24_5D132B0E_gradient_runningtitle.csv"]
+
+textCSV=["242_0_2638730_0_5D1327B5_gradient_textblock.csv",
+            "242_10_2638730_19_5D132A6D_gradient_textblock.csv",
+            "242_12_2638730_20_5D132A8C_gradient_textblock.csv",
+            "242_14_2638730_22_5D132AD1_gradient_textblock.csv",
+            "242_16_2638730_24_5D132B0E_gradient_textblock.csv"]
---
+++ struct_classes.py
@@ -0,0 +1,7 @@
+from PIL import Image, ImageColor
+from sklearn import preprocessing
+import numpy as np
+import scipy as sp
+from app_config import *
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
